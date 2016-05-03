34908f08de3596ba266528e01aa19461de30649a
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-03 23:39:57 +0300

    [Feature] Changed registration

--- bucketlist/app/models.py
+++ bucketlist/app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserProfile(User):
+class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
     user = models.OneToOneField(User)
 
--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -31,14 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'app',
-    'rest_framework',
+    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
+    'app',
+    'rest_framework',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +123,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
