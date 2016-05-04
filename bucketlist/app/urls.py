45736114b116a8076f619ba5627de90fef561b09
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-04 23:06:37 +0300

    [Feature] Added models and edited urls

--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
-
-from views import authentication_view
+from django.conf.urls import url, include
+from app.views import register_view, login_view, logout_view
 
 urlpatterns = [
-    # url(r'^$', authentication_view.index, name='index'),
-    # url(r'^$', authentication_view.IndexView.as_view(), name='index'),
-    # url(r'^auth/register', views.create_user)
-    url(r'^$', authentication_view.UserFormView, name='register'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^register/$', 'app.views.register_view'),
+    url(r'^login/$', 'app.views.login_view'),
+    url(r'^logout/$', 'app.views.logout_view'),
+    url(r'^api/', include('api.urls')),
 ]
