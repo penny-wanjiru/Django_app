145345977c3d015cfffa4cdaf80a28388568efd6
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-05 15:40:00 +0300

    [Feature] added search query feature to the api

--- bucketlist/app/admin.py
+++ bucketlist/app/admin.py
@@ -1,4 +1,21 @@
 from django.contrib import admin
+from .models import BucketList, BucketListItem
+
 
 # Register your models here.
+class BucketListAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
 
+
+class BucketListItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bucketlist', 'done')
+    list_display_links = ('name',)
+    search_fields = ('name',)
+    list_per_page = 25
+
+
+admin.site.register(BucketList, BucketListAdmin)
+admin.site.register(BucketListItem, BucketListItemAdmin)
