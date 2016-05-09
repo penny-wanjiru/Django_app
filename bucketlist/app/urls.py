3ff8b6e12e1ad6264f2bcaafdb28ab6910baf866
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-09 12:09:39 +0300

    [Fix] fixed login url errors

--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -3,8 +3,8 @@ from app.views import register_view, login_view, logout_view
 
 urlpatterns = [
     # url(r'^$', views.index, name='index'),
-    url(r'^register/$', 'app.views.register_view'),
-    url(r'^login/$', 'app.views.login_view'),
-    url(r'^logout/$', 'app.views.logout_view'),
+    url(r'^register/$', register_view.as_view()),
+    url(r'^login/$', login_view.as_view()),
+    url(r'^logout/$', logout_view.as_view()),
     url(r'^api/', include('api.urls')),
 ]
