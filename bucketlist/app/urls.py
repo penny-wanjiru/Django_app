d11c5080036dc8c6c5018199054e8a36e09d7b93
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-11 22:40:23 +0300

    [Feature] Create frontend files to display bucketlists and items

--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from app.views import register_view, login_view, logout_view
+from app.views import register_view, login_view, logout_view, index_view
 
 urlpatterns = [
-    # url(r'^$', views.index, name='index'),
+    url(r'^$', index_view.as_view(), name='index'),
     url(r'^register/$', register_view.as_view()),
     url(r'^login/$', login_view.as_view()),
     url(r'^logout/$', logout_view.as_view()),
