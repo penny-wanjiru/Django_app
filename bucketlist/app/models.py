31187025957a5e996eddca4660b4755b65f676c2
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-12 23:51:59 +0300

    [Fix] fixed login errors

--- bucketlist/app/models.py
+++ bucketlist/app/models.py
@@ -6,6 +6,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class SignUp(models.Model):
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    password_two = models.CharField(max_length=255, default='password')
+
+    def __unicode__(self):
+        return u'%s' % self.email
+
+
+class SignIn(models.Model):
+    username = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+
+    def __unicode__(self):
+        return u'%s' % self.username        
+
+
 class BucketList(models.Model):
     name = models.CharField(unique=True, max_length=255,
                             default="BucketList")
--- bucketlist/app/views.py
+++ bucketlist/app/views.py
@@ -5,19 +5,33 @@ from django.contrib.auth import(
     login,
     logout,
 ) 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.shortcuts import render, redirect
-from .forms import UserRegistrationForm, UserLoginForm
+from .forms import UserRegistrationForm, UserLoginForm, SignUpForm
 
 
-class index_view(TemplateView):
-    template_name = "bucketlists.html"
+class index_view(View):
+    def get(self, request):
+        form = SignUpForm(None)
+        context = {"form": form}
+        return render(request, 'Signup.html', context)
+
+    def post(self, request):
+        print request.POST
+        form = SignUpForm(request.POST or None)
+        if form.is_valid():
+            form.save()
+            return redirect("login/")
+        context = {"form": form}
+        return render(request, 'Signup.html', context)
+
+
+class login_view(View):
 
     def get(self, request):
-        return render(request, 'bucketlists.html')
-
-
-class login_view(TemplateView):
+        form = UserLoginForm(None)
+        context = {"form": form}
+        return render(request, 'userform.html', context)
 
     def post(self, request):
         title = "login"
@@ -26,9 +40,10 @@ class login_view(TemplateView):
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
             user = authenticate(username=username, password=password)
+            print 'niga is a`ight'
             login(request, user)
             return redirect("/")
-        return render(request, "new.html", {"form": form, "title": title})
+        return render(request, "userform.html", {"form": form, "title": title})
 
 
 class logout_view(TemplateView):
@@ -41,7 +56,7 @@ class logout_view(TemplateView):
 class register_view(TemplateView):
     template_name = "new.html"
 
-    def post(request):
+    def post(self, request):
         title = "Register"
         form = UserRegistrationForm(request.POST or None)
         if form.is_valid():
