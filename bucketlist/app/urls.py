4f1539cd95cc456cb7cf35147b1381e2c8eaa646
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-15 21:15:07 +0300

    [Fix] redirects to all pages

--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url, include
-from app.views import register_view, login_view, logout_view, index_view, bucketlist_view, BucketlistCreate
+from app.views import login_view, logout_view, index_view, BucketlistView
 
 urlpatterns = [
     url(r'^$', index_view.as_view(), name='index'),
-    url(r'^register/$', register_view.as_view()),
     url(r'^login/$', login_view.as_view()),
     url(r'^logout/$', logout_view.as_view()),
-    url(r'^bucketlists/$', bucketlist_view.as_view()),
-    url(r'^bucketlist/add$', BucketlistCreate.as_view()),
+    url(r'^bucketlists/$', BucketlistView.as_view(), name="bucket_add"),
     url(r'^api/', include('api.urls')),
 ]
