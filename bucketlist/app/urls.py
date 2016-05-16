d9ede7bbb409aa7a183cfb23ea37cc8d34d547f4
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-16 23:34:23 +0300

    [Feature] added done checkbox for bucketlist items

--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import url, include
-from app.views import login_view, logout_view, index_view, BucketlistView, BucketlistItemsView, BucketlistDelete
+from app.views import login_view, logout_view, index_view, BucketlistView, BucketlistItemsView, BucketlistItemStatus
 
 urlpatterns = [
     url(r'^$', index_view.as_view(), name='index'),
     url(r'^login/$', login_view.as_view()),
     url(r'^logout/$', logout_view.as_view()),
-    url(r'^bucketlist/(?P<pk>[0-9]+)/items/$', BucketlistItemsView.as_view(), name="bucket_items"),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDelete.as_view(), name="bucket_del"),
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$', BucketlistItemsView.as_view(), name="bucket_items"),
+    url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<pk>[0-9]+)/status/$',
+        BucketlistItemStatus.as_view(),
+        name="bucketlistitems_status"),
+    # url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDelete.as_view(), name="bucket_del"),
     url(r'^bucketlists/$', BucketlistView.as_view(), name="bucket_add"),
     # url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistUpdate.as_view(), name="bucket_up"),
     
