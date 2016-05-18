494ed841bc171a29f881eb22c0e579dde497b70c
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-18 22:35:13 +0300

    [Feature] added sweet alert library

--- bucketlist/app/static/js/my_js.js
+++ bucketlist/app/static/js/my_js.js
@@ -2,3 +2,40 @@ $(".btn-edit").on("click", function(event) {
   var dataUrl = $(this).data("url");
   $("#updateBucketList").attr('action', dataUrl);
 });
+
+$(".btn-item").on("click", function(event) {
+  var dataUrl = $(this).data("url");
+  $("#updateitem").attr('action', dataUrl);
+});
+
+$(".delete-item").on("click", function(event) {
+      event.preventDefault();
+        var a = $(this).attr('link'); 
+        console.log(a); 
+
+     swal(
+         {   
+            title: "Are you sure?",   
+            text: "You will not be able to recover this imaginary file!",   
+            type: "warning",   
+            showCancelButton: true,   
+            confirmButtonColor: "#DD6B55",   
+            confirmButtonText: "Yes, delete it!",   
+            cancelButtonText: "No, cancel plx!",   
+            closeOnConfirm: false,   
+            closeOnCancel: false 
+        }, 
+        function(isConfirm) {
+            if (isConfirm) {
+                $.get( a, function( data ) {
+                  
+                }); 
+                swal("Deleted!", "Your imaginary file has been deleted.", "success");   
+            } 
+            else {     
+                swal("Cancelled", "Your imaginary file is safe :)", "error");  
+             } 
+            location.reload();
+        });
+});
+
--- bucketlist/app/urls.py
+++ bucketlist/app/urls.py
@@ -1,23 +1,41 @@
 from django.conf.urls import url, include
-from app.views import login_view, logout_view, index_view, BucketlistView, BucketlistItemsView, BucketlistItemStatus, BucketlistDeleteView, BucketlistUpdateView
+from app.views import (login_view,
+ logout_view, index_view, BucketlistView,
+ BucketlistItemsView, BucketlistItemStatus,
+ BucketlistDeleteView, BucketlistUpdateView,
+ BucketlistItemDelete, BucketlistItemUpdate
+ )
 
 urlpatterns = [
     url(r'^$', index_view.as_view(), name='index'),
     url(r'^login/$', login_view.as_view()),
     url(r'^logout/$', logout_view.as_view()),
-    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$', BucketlistItemsView.as_view(), name="bucket_items"),
+
+    url(r'^bucketlists/(?P<pk>[0-9]+)/items/$',
+        BucketlistItemsView.as_view(),
+        name="bucket_items"),
+
     url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<pk>[0-9]+)/status/$',
         BucketlistItemStatus.as_view(),
         name="bucketlistitems_status"),
+
     url(r'^bucketlists/(?P<pk>[0-9]+)/delete/$',
         BucketlistDeleteView.as_view(),
         name="bucketlist_delete"),
+
     url(r'^bucketlists/(?P<pk>[0-9]+)/edit/$',
         BucketlistUpdateView.as_view(),
         name="bucketlist_edit"),
-    # url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistDelete.as_view(), name="bucket_del"),
+
+    url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<pk>[0-9]+)/delete/$',
+        BucketlistItemDelete.as_view(),
+        name="bucketlistitems_delete"),
+
+    url(r'^bucketlists/(?P<bucketlist>[0-9]+)/items/(?P<pk>[0-9]+)/edit/$',
+        BucketlistItemUpdate.as_view(),
+        name="items_edit"),
+
     url(r'^bucketlists/$', BucketlistView.as_view(), name="bucket_add"),
-    # url(r'^bucketlists/(?P<pk>[0-9]+)/$', BucketlistUpdate.as_view(), name="bucket_up"),
-    
+
     url(r'^api/', include('api.urls')),
 ]
