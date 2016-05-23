f5b9b1bc82033e9ad229838e5dd900abcb83194c
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-23 14:26:05 +0300

    [Fix] added static settings

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -135,6 +135,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT= os.path.join(BASE_DIR,'staticfiles')
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 STATIC_URL = '/static/'
-
-
