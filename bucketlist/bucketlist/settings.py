6b5cac16bba415eaf0e0d5d85a412fece28f2aaa
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-23 23:02:30 +0300

    [Fix] Set heroku db credentials

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 import os.path
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -91,6 +92,11 @@ DATABASES = {
         'PASSWORD': 'Andela2015'
     }
 }
+
+if 'ON_HEROKU' in os.environ:
+    print 'Detected on Heroku.'
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env)
 
 TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
