35841f644f914a3cd4f97571ade29676f7b8c887
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-23 14:10:35 +0300

    [fix] changed requirements

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/penny/projects/bucketlistapp/venv"
+set -gx VIRTUAL_ENV "/Users/penny/projects/Django_app/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
