7084b62beaf45bcb283cda2f0720174ef885a210
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-23 17:32:08 +0300

    [Fix] add template settings

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -131,6 +131,21 @@ USE_L10N = True
 
 USE_TZ = True
 
+# here() gives us file paths from the root of the system to the directory
+# holding the current file.
+here = lambda * x: os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+
+PROJECT_ROOT = here("..")
+# root() gives us file paths from the root of the system to whatever
+# folder(s) we pass it starting at the parent directory of the current file.
+root = lambda * x: os.path.join(os.path.abspath(PROJECT_ROOT), *x)
+
+....
+
+TEMPLATE_DIRS = (
+    root('templates'),
+)
+
 
 
 # Static files (CSS, JavaScript, Images)
