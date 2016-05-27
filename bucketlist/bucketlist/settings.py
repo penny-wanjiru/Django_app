948171e0fca080607bf84ee6052b70062a01070e
Penina Wanjiru <penina.wanjiru@andela.com>
2016-05-27 10:31:58 +0300

    [fix] docs in installed apps

--- bucketlist/bucketlist/settings.py
+++ bucketlist/bucketlist/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'rest_framework_docs'
+    'rest_framework_docs',
     'app',
     'api',
 ]
