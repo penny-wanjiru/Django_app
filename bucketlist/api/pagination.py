f1ffc535b8140a16b3d80014ee49f4a207484f49
Penina Wanjiru <penina.wanjiru@andela.com>
2016-06-02 22:28:59 +0300

    [Chore] add limit pagination

--- bucketlist/api/pagination.py
+++ bucketlist/api/pagination.py
@@ -1,13 +1,8 @@
 from rest_framework.pagination import (
-    PageNumberPagination,
     LimitOffsetPagination
 )
 
 
-class BucketlistPageNumberPagination(PageNumberPagination):
-    page_size = 2
-
-
 class BucketlistLimitOffsetPagination(LimitOffsetPagination):
     default_limit = 2
-    max_limit = 2
+    max_limit = 7
