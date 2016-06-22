9cd96f502920c83e651a1fa1f0e2266510341780
Penina Wanjiru <penina.wanjiru@andela.com>
2016-06-22 11:49:28 +0300

    [Fix] fix indentation issues

--- bucketlist/static/js/app.js
+++ bucketlist/static/js/app.js
@@ -9,66 +9,60 @@ $(".btn-item").on("click", function(event) {
 });
 
 $(".delete-item").on("click", function(event) {
-      event.preventDefault();
-        var a = $(this).attr('link'); 
-    swal(
-        {   
-            title: "Are you sure?",   
-            text: "You will not be able to recover this item!",   
-            type: "warning",   
-            showCancelButton: true,   
-            confirmButtonColor: "#c62828",   
-            confirmButtonText: "Yes, delete it!",   
-            cancelButtonText: "No, cancel!",   
-            closeOnConfirm: false,   
-            closeOnCancel: false 
-        }, 
-        function(isConfirm) {
-            if (isConfirm) {
-                $.get(a); 
-                swal("Deleted!", "Your item has been deleted.", "success");   
-            } 
-            else {     
-                swal("Cancelled", "Your item is still safe", "error");  
-             } 
-            location.reload();
-        });
+  event.preventDefault();
+  var a = $(this).attr('link');
+  swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this item!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#c62828",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      closeOnConfirm: false,
+      closeOnCancel: false
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        $.get(a);
+        swal("Deleted!", "Your item has been deleted.", "success");
+      } else {
+        swal("Cancelled", "Your item is still safe", "error");
+      }
+      location.reload();
+    });
 });
 
 $(".delete-bucket").on("click", function(event) {
-      event.preventDefault();
-        var a = $(this).attr('links');
-
-    swal(
-         {   
-            title: "Are you sure?",   
-            text: "You will not be able to recover this item!",   
-            type: "warning",   
-            showCancelButton: true,   
-            confirmButtonColor: "#c62828",   
-            confirmButtonText: "Yes, delete it!",   
-            cancelButtonText: "No, cancel!",   
-            closeOnConfirm: false,   
-            closeOnCancel: false 
-        }, 
-        function(isConfirm) {
-            if (isConfirm) {
-                $.get(a);
-                swal("Deleted!", "Your item has been deleted.", "success");   
-            } 
-            else {     
-                swal("Cancelled", "Your item is still safe", "error");  
-             } 
-            location.reload();
-        });
+  event.preventDefault();
+  var a = $(this).attr('links');
+  swal({
+      title: "Are you sure?",
+      text: "You will not be able to recover this item!",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#c62828",
+      confirmButtonText: "Yes, delete it!",
+      cancelButtonText: "No, cancel!",
+      closeOnConfirm: false,
+      closeOnCancel: false
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        $.get(a);
+        swal("Deleted!", "Your item has been deleted.", "success");
+      } else {
+        swal("Cancelled", "Your item is still safe", "error");
+      }
+      location.reload();
+    });
 });
 
-$(document).ready(function(){
-        $('.modal-trigger').leanModal();
-        $(".flash-message").fadeOut(4000);       
+$(document).ready(function() {
+  $('.modal-trigger').leanModal();
+  $(".flash-message").fadeOut(4000);
 });
 
-function sendCheck (itemId, status) {
-    window.location.href += itemId + '/status';
+function sendCheck(itemId, status) {
+  window.location.href += itemId + '/status';
 }
-
